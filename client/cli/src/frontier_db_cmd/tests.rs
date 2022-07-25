44eb4a29b80ca92587e020d92be67c22b0f24305
Kent Jin <zjin@deeper.network>
2022-07-24 22:23:28 -0700

    1. changes of hotfix-sufficients pallet for cargo test 2. github workflows setup for dev/polkadot-v** branches

--- client/cli/src/frontier_db_cmd/tests.rs
+++ client/cli/src/frontier_db_cmd/tests.rs
@@ -541,7 +541,7 @@ mod tests {
 		let mut client = Arc::new(c);
 
 		// Get some transaction status.
-		let t1 = fp_rpc::TransactionStatus::default();
+		let t1 = fp_rpc::TransactionStatusV2::default();
 		let t1_hash = t1.transaction_hash;
 		let statuses = vec![t1];
 
@@ -610,8 +610,8 @@ mod tests {
 		let mut client = Arc::new(c);
 
 		// Get some transaction status.
-		let t1 = fp_rpc::TransactionStatus::default();
-		let mut t2 = fp_rpc::TransactionStatus::default();
+		let t1 = fp_rpc::TransactionStatusV2::default();
+		let mut t2 = fp_rpc::TransactionStatusV2::default();
 		t2.transaction_hash =
 			H256::from_str("0x2200000000000000000000000000000000000000000000000000000000000000")
 				.unwrap();
@@ -746,7 +746,7 @@ mod tests {
 		let mut client = Arc::new(c);
 
 		// Get some transaction status.
-		let t1 = fp_rpc::TransactionStatus::default();
+		let t1 = fp_rpc::TransactionStatusV2::default();
 		let t1_hash = t1.transaction_hash;
 		let statuses = vec![t1];
 
